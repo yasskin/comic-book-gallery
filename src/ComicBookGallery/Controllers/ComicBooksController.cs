f9199d7c9f701a964201680c70d5c5055c3b4403
Noah Yasskin <noahyasskin@gmail.com>
2017-11-21 14:11:48 -0800

    Updated comic book view detail layout

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -23,7 +23,7 @@ namespace ComicBookGallery.Controllers
                     new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
                     new Artist() { Name = "Victor Olazaba", Role = "Inks"},
                     new Artist() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"}
                 }
 ,            };
 
--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -27,7 +27,7 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace("", "-").ToLower() + "-" + IssueNumber + ".jpg";
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
             }
         }
     }
